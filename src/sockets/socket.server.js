05729d862d27421e3a8ebf09dcf6d37a5af1cd8a
Ardhendu Abhishek Meher <meherardhenduabhishek@gmail.com>
2025-08-18 22:28:16 +0530

    Made a auth middleware for socket.io

--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -1,4 +1,7 @@
 const { Server } = require('socket.io');
+const cookie = require('cookie')
+const jwt = require('jsonwebtoken');
+const userModel = require('../models/user.model')
 
 function initSocketServer(httpServer) {
 
@@ -9,7 +12,24 @@ function initSocketServer(httpServer) {
 
         /* Middlewware to check the user is loggedin or not */
         io.use(async (socket, next) => {
-            
+            const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+
+            if(!cookie.token){
+                next(new Error("Authentication error: No token provided"));
+            }
+
+            try{
+
+                const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+
+                const user = await userModel.findOne(decoded.id);
+
+                socket.user = user;
+
+                next();
+            }catch(err){
+                next(new Error("Authentication error: Invalid token"))
+            }
         })
 
         socket.on("disconnect", () => {
