797b74edc7e25a37c107182ad7625b8e51383904
Ardhendu Abhishek Meher <meherardhenduabhishek@gmail.com>
2025-09-08 22:31:51 +0530

    Add new 96x96 icon for the frontend

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,6 +5,11 @@ import './App.css'
 import 'katex/dist/katex.min.css';
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { registerSW } from 'virtual:pwa-register'
+
+if (import.meta.env.PROD) {
+  registerSW()
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
